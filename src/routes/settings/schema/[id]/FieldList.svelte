9055f740a202a7a224a7612293a5c0daf7973e80
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-11-02 17:01:12 +0000

    fix: field sorting

--- src/routes/settings/schema/[id]/FieldList.svelte
+++ src/routes/settings/schema/[id]/FieldList.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { faBars, faChevronRight, faPlus } from "@fortawesome/free-solid-svg-icons";
+	import { SortableList, sortItems } from "@rodrigodagostino/svelte-sortable-list";
 	import { createAccordion, melt } from "@melt-ui/svelte";
-	import type { Attachment } from "svelte/attachments";
 	import type { SchemaDoc } from "$lib/pouchdb/types";
 	import FieldForm from "./FieldForm.svelte";
 	import { slide } from "svelte/transition";
-	import Sortable from "sortablejs";
 	import Fa from "svelte-fa";
 
 	let { schema = $bindable() }: { schema: SchemaDoc } = $props();
@@ -15,22 +14,6 @@
 		helpers: { isSelected },
 	} = createAccordion();
 
-	const sortable: Attachment<HTMLDivElement> = (element) => {
-		const sortable = new Sortable(element, {
-			dragClass: "sortable-ghost",
-			handle: ".handle",
-			animation: 150,
-			onEnd(event) {
-				if (event.oldIndex === undefined || event.newIndex === undefined) return;
-				const movedItem = schema.fields.splice(event.oldIndex, 1)[0];
-				schema.fields.splice(event.newIndex, 0, movedItem);
-				schema.fields = schema.fields;
-			},
-		});
-
-		return sortable.destroy.bind(sortable);
-	};
-
 	function add() {
 		schema.fields.push({
 			name: "Unnamed Field",
@@ -38,29 +21,41 @@
 			defaultValue: { type: "none" },
 		});
 	}
+
+	function ondragend(e: SortableList.RootEvents["ondragend"]) {
+		const { draggedItemIndex, targetItemIndex, isCanceled } = e;
+		if (!isCanceled && typeof targetItemIndex === "number" && draggedItemIndex !== targetItemIndex)
+			schema.fields = sortItems(schema.fields, draggedItemIndex, targetItemIndex);
+	}
 </script>
 
 <div class="container">
-	<div {...$root} {@attach sortable}>
-		{#each schema.fields as field, i (field.name)}
-			{@const props = { value: i.toString() }}
-			<div use:melt={$item(props)} data-id={field.name}>
-				<div style="display: flex; align-items: center">
-					<button use:melt={$trigger(props)}>
-						<span class="chevron" class:rotated={$isSelected(props.value)}>
-							<Fa icon={faChevronRight} />
-						</span>
-						{field.name.length > 0 ? field.name : "Unnamed Field"}
-					</button>
-					<span class="handle"><Fa icon={faBars} /></span>
-				</div>
-				{#if $isSelected(props.value)}
-					<div use:melt={$content(props)} transition:slide class="content">
-						<FieldForm bind:field={schema.fields[i]} />
+	<div {...$root}>
+		<SortableList.Root gap={0} {ondragend} hasLockedAxis>
+			{#each schema.fields as field, i (i)}
+				{@const props = { value: i.toString() }}
+				<SortableList.Item id={field.name} index={i}>
+					<div use:melt={$item(props)} class="field">
+						<div style="display: flex; align-items: center">
+							<button use:melt={$trigger(props)}>
+								<span class="chevron" class:rotated={$isSelected(props.value)}>
+									<Fa icon={faChevronRight} />
+								</span>
+								{field.name.length > 0 ? field.name : "Unnamed Field"}
+							</button>
+							<SortableList.ItemHandle>
+								<span class="handle"><Fa icon={faBars} /></span>
+							</SortableList.ItemHandle>
+						</div>
+						{#if $isSelected(props.value)}
+							<div use:melt={$content(props)} transition:slide class="content">
+								<FieldForm bind:field={schema.fields[i]} />
+							</div>
+						{/if}
 					</div>
-				{/if}
-			</div>
-		{/each}
+				</SortableList.Item>
+			{/each}
+		</SortableList.Root>
 	</div>
 	<button onclick={add}>
 		<Fa icon={faPlus} /> Add Field
@@ -72,6 +67,10 @@
 		background-color: var(--bg-2);
 		border-radius: 5px;
 		overflow: hidden;
+	}
+
+	.field {
+		background-color: var(--bg-2);
 	}
 
 	button {
@@ -101,12 +100,17 @@
 		padding: 0 10px;
 	}
 
-	.sortable-ghost {
+	:global(.ssl-ghost .field) {
 		background-color: var(--bg-3);
+		opacity: 1 !important;
 		border-radius: 5px;
 
-		.content {
+		:global(.content) {
 			display: none;
 		}
+	}
+
+	:global(.ssl-item[data-drag-state="ptr-drag"]) > .field {
+		opacity: 0.25;
 	}
 </style>
