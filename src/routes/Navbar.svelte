3bfad9fdce35b05838cc9e339ffe732ddd11535f
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-11-27 23:40:57 +0000

    fix: navbar padding

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -18,7 +18,7 @@
 	let margin = $state(0);
 </script>
 
-<div style="height: {margin}px;"></div>
+<div style="height: {margin + 10}px;"></div>
 <nav style="padding-bottom: {$padding}px;" bind:clientHeight={margin}>
 	{#each entries as { href, icon, label }}
 		<a {href} class:active={page.data.path[0] == href.split("/")[1]}>
