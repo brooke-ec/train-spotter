7febe654783d260eb9691e03ba61122beeb98c70
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-11-27 23:16:28 +0000

    fix: ListInput empty values

--- src/lib/components/ListInput.svelte
+++ src/lib/components/ListInput.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
 	import { slide } from "svelte/transition";
 
-	let { value = $bindable(), placeholder = "value" }: { value: string[]; placeholder?: string } = $props();
+	let { value = $bindable([]), placeholder = "value" }: { value: string[]; placeholder?: string } =
+		$props();
 	let inputs: HTMLInputElement[] = $state([]);
-	value = value ?? [];
+	let raw: string[] = $state(value); // todo: not two way reactive :(
 
 	$effect(() => {
-		if (!value.some((i) => i == "")) value.push("");
+		if (!raw.some((i) => i == "")) raw.push("");
+	});
+
+	$effect(() => {
+		value = raw.filter((i) => i != "");
 	});
 
 	function keyDown(e: KeyboardEvent, i: number) {
 		// Keyboard Navigation
 		if (e.key == "ArrowUp" && i > 0) focus(i - 1);
-		else if (["Enter", "ArrowDown"].includes(e.key) && i < value.length - 1) focus(i + 1);
+		else if (["Enter", "ArrowDown"].includes(e.key) && i < raw.length - 1) focus(i + 1);
 		// Delete empty input when backspace pressed
-		else if (["Backspace", "Delete"].includes(e.key) && value[i] == "" && value.length > 1) {
+		else if (["Backspace", "Delete"].includes(e.key) && raw[i] == "" && raw.length > 1) {
 			// If attempting to delete the last input, only allow if there are other empty inputs
-			if (i != value.length - 1 || value.filter((i) => i == "").length > 1) value.splice(i, 1);
+			if (i != raw.length - 1 || raw.filter((i) => i == "").length > 1) raw.splice(i, 1);
 			focus(Math.max(0, i - 1));
 		}
 	}
@@ -25,12 +30,12 @@
 </script>
 
 <div class="container">
-	{#each value as _, i}
+	{#each raw as _, i}
 		<input
 			type="text"
 			{placeholder}
 			transition:slide
-			bind:value={value[i]}
+			bind:value={raw[i]}
 			bind:this={inputs[i]}
 			onkeydown={(e) => keyDown(e, i)}
 		/>
